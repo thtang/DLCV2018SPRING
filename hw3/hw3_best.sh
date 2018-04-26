8cacb56434ba4fe6da90d0702e780493ac82a80d
T.H. Tang <penniless1994@gmail.com>
2018-04-26 18:23:23 +0800

    mod model

--- hw3/hw3_best.sh
+++ hw3/hw3_best.sh
@@ -1,2 +1,2 @@
 wget -O fcn8s_25ep_model.pkt https://www.dropbox.com/s/bnaqrblcpu42aqq/fcn8s_25ep_model.pkt?dl=0
-python3 fcn32_inference.py 0 ./fcn32s_25ep_model.pkt $1 $2
+python3 fcn32_inference.py 0 ./fcn8s_25ep_model.pkt $1 $2
